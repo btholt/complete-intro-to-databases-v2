6fc238daa4a029c3eb66460ea608cc3109b36bc9
Brian Holt <btholt@gmail.com>
2026-02-07 11:17:45 -0800

    initial commit :octocat:

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,12 @@ function Layout({ children }) {
   return (
     <CourseInfoProvider value={courseInfo}>
       <HeaderProvider value={headerHook}>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+          rel="stylesheet"
+        />
         <div className="remix-app">
           <Header title={courseInfo.title} />
           <div className="content-container">
@@ -24,6 +30,14 @@ function Layout({ children }) {
             bluesky={courseInfo.social.bluesky}
           />
         </div>
+        <script async defer src="https://a.holt.courses/latest.js"></script>
+        <noscript>
+          <img
+            src="https://a.holt.courses/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
       </HeaderProvider>
     </CourseInfoProvider>
   );
